12c6ae24bc6dd27d7da0ec1612613b4a853d60c1
LiquidatorCoder <maurya.abhay30@gmail.com>
2020-02-07 23:04:18 +0530

    Added ADB tap command, takes 1.5 secs for each tap

--- main.py
+++ main.py
@@ -13,22 +13,12 @@ for i in range(10):
 	subprocess.call("adb shell screencap -p | perl -pe 's/\x0D\x0A/\x0A/g' > screenshot.png",shell=True)
 	large_image = cv2.imread('screenshot.png')
 	result = cv2.matchTemplate(small_image, large_image, method)
-	# We want the minimum squared difference
 	mn,_,mnLoc,_ = cv2.minMaxLoc(result)
-
-	# Draw the rectangle:
-	# Extract the coordinates of our best match
-	MPx,MPy = mnLoc
-
-	# Step 2: Get the size of the template. This is the same size as the match.
-	trows,tcols = small_image.shape[:2]
-
-	# Step 3: Draw the rectangle on large_image
-	cv2.rectangle(large_image, (MPx,MPy),(MPx+tcols,MPy+trows),(0,0,255),2)
-
-	# Display the original image with the rectangle around the match.
-	cv2.imshow('output',large_image)
-
-	# The image is only displayed if we call this
-	cv2.waitKey(0)
+	x,y = mnLoc
+	print(x,y)
+	subprocess.call(f"adb shell input touchscreen tap {x} {y}",shell=True)
+	#trows,tcols = small_image.shape[:2]
+	#cv2.rectangle(large_image, (x,y),(x+tcols,y+trows),(0,0,255),2)
+	#cv2.imshow('output',large_image)
+	#cv2.waitKey(0)
 	print(time.time() - start)
